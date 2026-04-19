d9e27aded478b6750287559d9e208c64c61b4992
Zahra Aledavood <zarsdt@gmail.com>
2026-04-19 14:35:39 +0330

    [BUILD]

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -41,7 +41,8 @@ const Home = () => {
         </div>
         <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
             <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
-                <a className="w-full flex items-center justify-center px-8 py-3 border border-transpaent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10" href="https://drive.google.com/file/d/1zEA0lmGDmr93_fi7vy6Henn3iSeR4tHh/view?usp=sharing">
+                <a className="w-full flex items-center justify-center px-8 py-3 border border-transpaent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
+                 href="https://mstorage2.jobinjacdn.com/records/files/uploads/documents/ad1f32c8-f88a-45dd-9d9b-38c019465ff4.pdf?requester=322e3137382e36352e3935&resource=753a31363731303936&from=6170706c69636174696f6e5f666f726d&X-Amz-Content-Sha256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=oetSom924MQvtCRu9N6vvQ4bt8cG9HSpRnxWU756SyWy5NVt6nTpmfA9aV2b%2F20260419%2F%2Fs3%2Faws4_request&X-Amz-Date=20260419T110112Z&X-Amz-SignedHeaders=host&X-Amz-Expires=1800&X-Amz-Signature=a43330d658599b3f9f53ef2310da5d38a9e6da8b73dcd7a2d2172b4f142c2f97">
                     Resume
                 </a>
             </div>
